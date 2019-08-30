d0a8dc1a7f41a678f5a8f199059a44ae9d47b7af
Kevin Moonen <dragonrider7225@gmail.com>
2019-08-30 18:50:43 -0500

    Ran rustfmt

--- src/functor/mod.rs
+++ src/functor/mod.rs
@@ -1,42 +1,49 @@
 use core::*;
 
-pub trait Functor: Unplug+Plug<unplug!(Self, A)> {
-    fn map<B, F>(f:F, s:Self) -> plug!(Self[B]) where
-        Self:Plug<B>,
-        F:Fn(Self::A) -> B
-        ;
+pub trait Functor: Unplug + Plug<unplug!(Self, A)> {
+    fn map<B, F>(f: F, s: Self) -> plug!(Self[B])
+    where
+        Self: Plug<B>,
+        F: Fn(Self::A) -> B;
 }
 
 impl<A> Functor for Box<A> {
-    fn map<B,F>(f:F, s:Self) -> plug!(Self[B]) where
-        F:Fn(Self::A) -> B
-    { 
+    fn map<B, F>(f: F, s: Self) -> plug!(Self[B])
+    where
+        F: Fn(Self::A) -> B,
+    {
         Box::new(f(*s))
     }
 }
 
 impl<A> Functor for Vec<A> {
-    fn map<B,F>(f:F, s:Self) -> plug!(Self[B]) where
-        F:Fn(Self::A) -> B
-    { 
+    fn map<B, F>(f: F, s: Self) -> plug!(Self[B])
+    where
+        F: Fn(Self::A) -> B,
+    {
         s.into_iter().map(f).collect()
     }
 }
 
-
 impl<A> Functor for Option<A> {
-    fn map<B,F>(f:F, s:Self) -> plug!(Self[B]) where
-        F:Fn(Self::A) -> B
+    fn map<B, F>(f: F, s: Self) -> plug!(Self[B])
+    where
+        F: Fn(Self::A) -> B,
     {
         s.map(f)
     }
 }
 
 #[cfg(test)]
-fn it_compiles<F:Functor,A,B,C>(functor:F, fun:impl Fn(A)->B, fun2:impl Fn(B)->C) -> <F as Plug<C>>::result_t where
-    F:Plug<A>+Plug<B>+Plug<C>+Unplug<A=A>,
-    <F as Unplug>::F: Plug<A> + Plug<B> + Plug<C>
+fn it_compiles<F: Functor, A, B, C>(
+    functor: F,
+    fun: impl Fn(A) -> B,
+    fun2: impl Fn(B) -> C,
+) -> <F as Plug<C>>::result_t
+where
+    F: Plug<A> + Plug<B> + Plug<C> + Unplug<A = A>,
+    <F as Unplug>::F: Plug<A> + Plug<B> + Plug<C>,
 {
-    let cmp = |x|fun2(fun(x));
+    let cmp = |x| fun2(fun(x));
     Functor::map(cmp, functor)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,9 @@
+pub mod applicative;
 #[macro_use]
 pub mod core;
 pub mod functor;
-pub mod applicative;
-pub mod monad;
 pub mod impls;
-
+pub mod monad;
 
 #[cfg(test)]
 mod tests {
